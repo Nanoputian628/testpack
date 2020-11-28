e128098be9724b00a06b5373742ed9dd30694167
Nanoputian628 <dilshan.wijesena@hotmail.com>
2020-11-28 20:07:15 +1100

    Created first function called test_fun.

---
+++ R/test_fun.R
@@ -0,0 +1,18 @@
+#' @title test_fun
+#'
+#' @description First test function
+#'
+#' @param x A integer object
+#'
+#' @return Integer object, double input \code{test_fun}
+#' @examples
+#' in <- 3
+#' out <- test_fun(x = in)
+#' @export
+#' @importFrom dplyr "%>%"
+
+
+test_fun <- function(x) {
+  2*x
+}
+
